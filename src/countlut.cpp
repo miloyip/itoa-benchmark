629afd13cb73b325e76b9eaba2f12afba4caa78e
PublicScan <wyyxscan0@126.com>
2014-07-25 06:21:49 +0800

    Replace tab with spaces in countlut.cpp

--- src/countlut.cpp
+++ src/countlut.cpp
@@ -6,42 +6,42 @@
 // Use lookup table of two gDigitsLut
 
 void u32toa_countlut(uint32_t value, char* buffer) {
-	unsigned digit = CountDecimalDigit32(value);
-	buffer += digit;
-	*buffer = '\0';
+    unsigned digit = CountDecimalDigit32(value);
+    buffer += digit;
+    *buffer = '\0';
 
     const uint16_t* gDigitsLut16 = reinterpret_cast<const uint16_t*>(gDigitsLut);
 
-	while (value >= 100) {
+    while (value >= 100) {
         buffer -= 2;
         reinterpret_cast<uint16_t*>(buffer)[0] = gDigitsLut16[value % 100];
 
         value /= 100;
-	}
+    }
 
-	if (value < 10) {
+    if (value < 10) {
         --buffer;
-		buffer[0] = char(value) + '0';
-	}
-	else {
+        buffer[0] = char(value) + '0';
+    }
+    else {
         buffer -= 2;
         reinterpret_cast<uint16_t*>(buffer)[0] = gDigitsLut16[value];
-	}
+    }
 }
 
 void i32toa_countlut(int32_t value, char* buffer) {
-	if (value < 0) {
-		*buffer++ = '-';
-		value = -value;
-	}
-	
-	u32toa_countlut(static_cast<uint32_t>(value), buffer);
+    if (value < 0) {
+        *buffer++ = '-';
+        value = -value;
+    }
+
+    u32toa_countlut(static_cast<uint32_t>(value), buffer);
 }
 
 void u64toa_countlut(uint64_t value, char* buffer) {
-	unsigned digit = CountDecimalDigit64(value);
-	buffer += digit;
-	*buffer = '\0';
+    unsigned digit = CountDecimalDigit64(value);
+    buffer += digit;
+    *buffer = '\0';
 
     const uint16_t* gDigitsLut16 = reinterpret_cast<const uint16_t*>(gDigitsLut);
 
@@ -60,29 +60,29 @@ void u64toa_countlut(uint64_t value, char* buffer) {
     }
 
     uint32_t value32 = static_cast<uint32_t>(value);
-	while (value32 >= 100) {
+    while (value32 >= 100) {
         buffer -= 2;
         reinterpret_cast<uint16_t*>(buffer)[0] = gDigitsLut16[value32 % 100];
 
         value32 /= 100;
-	}
+    }
 
-	if (value32 < 10) {
+    if (value32 < 10) {
         --buffer;
-		buffer[0] = char(value32) + '0';
-	} else {
+        buffer[0] = char(value32) + '0';
+    } else {
         buffer -= 2;
         reinterpret_cast<uint16_t*>(buffer)[0] = gDigitsLut16[value32];
-	}
+    }
 }
 
 void i64toa_countlut(int64_t value, char* buffer) {
-	if (value < 0) {
-		*buffer++ = '-';
-		value = -value;
-	}
-	
-	u64toa_countlut(static_cast<uint64_t>(value), buffer);
+    if (value < 0) {
+        *buffer++ = '-';
+        value = -value;
+    }
+
+    u64toa_countlut(static_cast<uint64_t>(value), buffer);
 }
 
 REGISTER_TEST(countlut);
