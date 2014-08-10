27ee9a3f9c70a8c723dac102d4b4cb0639fef5eb
Milo Yip <miloyip@gmail.com>
2014-08-11 00:20:03 +0800

    Add pure C++ implementation of count decimal digit

--- src/countdecimaldigit.h
+++ src/countdecimaldigit.h
@@ -6,6 +6,7 @@
 #endif
 
 inline unsigned CountDecimalDigit32(uint32_t n) {
+#if defined(_MSC_VER) || defined(__GNUC__)
 	static const uint32_t powers_of_10[] = {
 		0,
 		10,
@@ -27,9 +28,23 @@ inline unsigned CountDecimalDigit32(uint32_t n) {
 	uint32_t t = (32 - __builtin_clz(n | 1)) * 1233 >> 12;
 #endif
 	return t - (n < powers_of_10[t]) + 1;
+#else
+	// Simple pure C++ implementation
+	if (n < 10) return 1;
+	if (n < 100) return 2;
+	if (n < 1000) return 3;
+	if (n < 10000) return 4;
+	if (n < 100000) return 5;
+	if (n < 1000000) return 6;
+	if (n < 10000000) return 7;
+	if (n < 100000000) return 8;
+	if (n < 1000000000) return 9;
+	return 10;
+#endif
 }
 
 inline unsigned CountDecimalDigit64(uint64_t n) {
+#if defined(_MSC_VER) || defined(__GNUC__)
 	static const uint64_t powers_of_10[] = {
 		0,
 		10,
@@ -50,7 +65,7 @@ inline unsigned CountDecimalDigit64(uint64_t n) {
 		10000000000000000,
 		100000000000000000,
 		1000000000000000000,
-		10000000000000000000U
+		10000000000000000000
 	};
 
 #if _M_IX86
@@ -68,6 +83,28 @@ inline unsigned CountDecimalDigit64(uint64_t n) {
 #elif __GNUC__
 	uint32_t t = (64 - __builtin_clzll(n | 1)) * 1233 >> 12;
 #endif
-
 	return t - (n < powers_of_10[t]) + 1;
+#else
+	// Simple pure C++ implementation
+	if (n < 10) return 1;
+	if (n < 100) return 2;
+	if (n < 1000) return 3;
+	if (n < 10000) return 4;
+	if (n < 100000) return 5;
+	if (n < 1000000) return 6;
+	if (n < 10000000) return 7;
+	if (n < 100000000) return 8;
+	if (n < 1000000000) return 9;
+	if (n < 10000000000) return 10;
+	if (n < 100000000000) return 11;
+	if (n < 1000000000000) return 12;
+	if (n < 10000000000000) return 13;
+	if (n < 100000000000000) return 14;
+	if (n < 1000000000000000) return 15;
+	if (n < 10000000000000000) return 16;
+	if (n < 100000000000000000) return 17;
+	if (n < 1000000000000000000) return 18;
+	if (n < 10000000000000000000) return 19;
+	return 20;
+#endif
 }
