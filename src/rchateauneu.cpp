19a6c50d086ca1bb04c1008c97bb151feb9612b9
Remi Chateauneu <remi.chateauneu@gmail.com>
2016-11-09 22:32:46 +0000

    Use 32 bits variables when smaller than 10000.

--- src/rchateauneu.cpp
+++ src/rchateauneu.cpp
@@ -302,9 +302,10 @@ char * ulltoa(char *aBuf, unsigned long long aLL)
 	const char * src = cstItoa + ATOI_TAB_SZ_LOG * aLL;
 	if (aLL < 10000)
 	{
-		if (aLL >= 1000) *aBuf++ = src[0];
-		if (aLL >= 100) *aBuf++ = src[1];
-		if (aLL >= 10) *aBuf++ = src[2];
+		unsigned int anInt = (unsigned int)aLL;
+		if (anInt >= 1000) *aBuf++ = src[0];
+		if (anInt >= 100) *aBuf++ = src[1];
+		if (anInt >= 10) *aBuf++ = src[2];
 		*aBuf++ = src[3];
 		*aBuf = '\0';
 		return aBuf;
