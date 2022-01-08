cfd0a5367ce409dbd4a3c32f799dce4dadc1e5a8
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-08 19:02:12 +0800

    Delete src directory

--- src/amartin.cpp
+++
@@ -1,9 +0,0 @@
-#include "itoa_ljust_impl.h"
-#include "test.h"
-
-void u32toa_amartin(uint32_t v, char* out) { (void)to_dec(out,v); }
-void u64toa_amartin(uint64_t v, char* out) { (void)to_dec(out,v); }
-void i32toa_amartin( int32_t v, char* out) { (void)to_dec(out,v); }
-void i64toa_amartin( int64_t v, char* out) { (void)to_dec(out,v); }
-
-REGISTER_TEST(amartin);
